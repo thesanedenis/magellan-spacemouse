9de3cfd54448f8f5b9b83a08a3913baf6cbd6c62
Denis Razumnyi <thesanedenis@gmail.com>
2026-04-17 00:34:14 +0200

    woring fine but buttons doesnt work correct

--- src/magellan.c
+++ src/magellan.c
@@ -13,9 +13,9 @@ uint16_t trans_report[3];
 uint16_t rot_report[3];
 uint8_t buttons_report[6];
 
-uint8_t trans_pending = 0;
-uint8_t rot_pending = 0;
-uint8_t buttons_pending = 0;
+volatile bool trans_pending = false;
+volatile bool rot_pending = false;
+volatile bool buttons_pending = false;
 
 uint8_t button_bits[] = { 12, 13, 14, 15, 22, 25, 23, 24, 0, 1, 2, 4, 5, 8, 26 };
 
@@ -30,6 +30,7 @@ int main(void)
     uart_set_format(UART_ID, 8, 1, UART_PARITY_NONE);
 
     sleep_ms(1000);
+
     uint8_t init_buf[] = { '\r', 'v', 'Q', '\r', 'm', '3', '\r' };
     uart_write_blocking(UART_ID, init_buf, sizeof(init_buf));
 
@@ -40,44 +41,41 @@ int main(void)
     {
         tud_task();
 
-        if (trans_pending && tud_hid_ready()) {
-            tud_hid_report(1, trans_report, 6);
-            trans_pending = 0;
-        }
-        if (rot_pending && tud_hid_ready()) {
-            tud_hid_report(2, rot_report, 6);
-            rot_pending = 0;
-        }
-        if (buttons_pending && tud_hid_ready()) {
-            tud_hid_report(3, buttons_report, 6);
-            buttons_pending = 0;
+        if (tud_hid_ready()) {
+            if (trans_pending) {
+                tud_hid_report(1, trans_report, 6);
+                trans_pending = false;
+            } else if (rot_pending) {
+                tud_hid_report(2, rot_report, 6);
+                rot_pending = false;
+            } else if (buttons_pending) {
+                tud_hid_report(3, buttons_report, 6);
+                buttons_pending = false;
+            }
         }
 
-        if (uart_is_readable(UART_ID)) {
+        while (uart_is_readable(UART_ID)) {
             uint8_t ch = uart_getc(UART_ID);
             buf[idx] = ch;
             idx = (idx + 1) % sizeof(buf);
             
             if (ch == '\r') {
                 if (idx >= 25 && buf[0] == 'd') {
-                    int16_t values[6];
+                    int16_t v[6];
                     for (int i = 0; i < 6; i++) {
-                        values[i] = -32768;
+                        v[i] = -32768;
                         for (int j = 0; j < 4; j++) {
-                            values[i] += (buf[1 + i * 4 + 3 - j] & 0xf) << (4 * j);
+                            v[i] += (buf[1 + i * 4 + 3 - j] & 0xf) << (4 * j);
                         }
                     }
-
-                    trans_report[0] = values[0];
-                    trans_report[1] = values[2];
-                    trans_report[2] = -values[1];
-                    
-                    rot_report[0] = values[3];
-                    rot_report[1] = values[5];
-                    rot_report[2] = -values[4];
-
-                    trans_pending = 1;
-                    rot_pending = 1;
+                    trans_report[0] = v[0];
+                    trans_report[1] = v[2];
+                    trans_report[2] = -v[1];
+                    rot_report[0] = v[3];
+                    rot_report[1] = v[5];
+                    rot_report[2] = -v[4];
+                    trans_pending = true;
+                    rot_pending = true;
                 } else if (idx >= 4 && buf[0] == 'k') {
                     uint16_t buttons = 0;
                     for (int i = 0; i < 3; i++) {
@@ -86,10 +84,10 @@ int main(void)
                     memset(buttons_report, 0, 6);
                     for (int i = 0; i < 15; i++) {
                         if (buttons & (1 << i)) {
-                            buttons_report[button_bits[i] / 8] |= 1 << (button_bits[i] % 8);
+                            buttons_report[button_bits[i] / 8] |= (1 << (button_bits[i] % 8));
                         }
                     }
-                    buttons_pending = 1;
+                    buttons_pending = true;
                 }
                 idx = 0;
             }
